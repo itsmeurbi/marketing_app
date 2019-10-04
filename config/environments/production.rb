4e06738f545985383433eb0da8f863844e24380b
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2019-10-03 20:06:16 -0500

    Configure production to use SendGrid as mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'miguel-urbina.me' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'miguel-urbina.me',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
