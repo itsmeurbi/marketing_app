5d76ca7fba1b896cbb2529ac0a21d5c54f57a8ee
15460193 <15460193@itcolima.edu.mx>
2019-10-16 14:23:04 -0500

    Update on new landing page with background image

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class LandingPageController < ApplicationController
+  layout 'landing-page-application'
+  
   def index
     @corporations = Corporation.all
     @companies = Company.all
