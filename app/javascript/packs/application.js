7d5b40b9a625cdba40d5f979fb00629989e133a1
Ramon Sanchez <joseramonsanchezsolorzano@gmail.com>
2019-10-23 20:22:36 -0500

    fix navbar-resposive menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("vis-network")
 require("axios")
 require("packs/notification")
 require("packs/vis")
+require("packs/navbar")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
