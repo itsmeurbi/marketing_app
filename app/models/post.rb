d440e3f316364ee0b169a7aee498a7a15a59b3cf
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2019-12-01 12:53:54 -0600

    Remove content status presence model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,6 @@ class Post < ApplicationRecord
   has_one_attached :content
   has_many :petitions
   validates :title, presence: true
-  validates :content_status, presence: true
   belongs_to :responsable, class_name: 'Coworker', foreign_key: 'coworker_id'
   enum content_status: %i[pending review approved]
   after_create :mark_node
