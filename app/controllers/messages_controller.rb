281148de04129c8e03ed6696ec2d093d7a0b64c6
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2019-12-01 20:39:18 -0600

    Append image src when sending broadcast channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,18 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image, :recipient_id).merge(user: current_user)
+    params.require(:message).permit(:content, :image, :recipient_id)
+          .merge(user: current_user)
   end
 
-  def send_user_message(recipient_id)
-    ActionCable.server.broadcast "chatroom_channel", author_email: current_user.email, content: @message.content
+  def send_user_message(_recipient_id)
+    image = if @message.image.attached?
+              url_for(@message.image)
+            else
+              ''
+            end
+    ActionCable.server.broadcast 'chatroom_channel',
+                                 author_email: current_user.email,
+                                 content: @message.content, image: image
   end
 end
--- db/migrate/20191201233457_add_participantsto_chatroom.rb
+++ db/migrate/20191201233457_add_participantsto_chatroom.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddParticipantstoChatroom < ActiveRecord::Migration[6.0]
   def change
     add_column :chatrooms, :first, :integer, null: false
