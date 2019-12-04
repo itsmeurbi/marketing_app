78010c1d0d49fd1d9b838bdb43ba8b6eb7c15bfe
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2019-12-03 21:07:15 -0600

    Send email when posting on facebook through rake task

---
+++ app/mailers/facebook_notifier_mailer.rb
@@ -0,0 +1,6 @@
+class FacebookNotifierMailer < ApplicationMailer
+  def facebook_post
+    @post = Post.find(params[:id])
+    mail(to: ENV['SUPPORT_MAIL'], subject: 'Se ha publicado nuevo contenido a la campaña')
+  end
+end
