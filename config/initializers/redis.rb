6b890023bae213f006e7a94d26811a285943afbc
Miguel Urbina <miguel.urbina15.1997@gmail.com>
2019-12-03 19:35:47 -0600

    Update web dependenci to use redistogo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-$redis = Resque.redis = Redis.new(url: ENV['REDIS_URL'])
+require 'resque/server'
+
+if Rails.env.development?
+  Resque.redis = Redis.new(:host => 'localhost', :port => '6379')
+else
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+  Resque.redis = REDIS
+end
